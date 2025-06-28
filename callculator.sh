47f8deb51036b532902e9ed9509794fcb4404645
soul1114 <cardosoinv@gmail.com>
2025-06-28 17:30:58 -0300

    git
    fim do modulo do git

---
+++ callculator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Este é o shebang, ele diz ao sistema para usar o Bash para executar este script.
+
+echo "--- Iniciando o cálculo de notas ---"
+
+python3 ./untitled1.py
+
+echo "--- Cálculo de notas finalizado! ---"
